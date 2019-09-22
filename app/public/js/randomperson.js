1722da27ba799fa72b043e541b10faafc7cea064
Simon Mantlo <smantlo@iu.edu>
2019-09-22 19:30:02 -0400

    Final commit

--- app/public/js/randomperson.js
+++ app/public/js/randomperson.js
@@ -10,7 +10,11 @@ var randomPerson = new Vue({
       .then( response => response.json() )
       .then( json => {randomPerson.user = json.results[0]} )
       ;
-    }
+    },
+    newUser(book) {
+      this.fetchUser();
+      console.log(book); //https://developer.mozilla.org/en-US/docs/Web/API/Console/log
+    } //consulted Alex Braunscheidel for structuring help
   },
     created() {
       this.fetchUser();
